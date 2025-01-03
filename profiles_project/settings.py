30eac0d6f621e203a88388a14f4cfbdf87b64f4f
chandan <chandankumar97@tutanota.com>
2025-01-03 20:34:03 +0530

    configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6+***91l41#back0ge0(c6oe1fbep&gj$e(iyi%1d09*%2b9%k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
 
 #DEFAULT_RENDERER_CLASSES
 
